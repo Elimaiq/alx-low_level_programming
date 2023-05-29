40c10a792493f7df59d6a88f5431e17216eb0172
Comfort Ogar <elimaiq@gmail.com>
2023-05-29 10:37:03 -0700

    Hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
+#include <studio.h>
+
+/**
+ *main - contains our code to prints our output
+ *Return - 0 (Successful execution)
+ */
+
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
