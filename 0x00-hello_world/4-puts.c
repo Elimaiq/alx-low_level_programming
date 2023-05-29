82c21f8809c1fce921a03ac189c8f16e29439a91
Comfort Ogar <elimaiq@gmail.com>
2023-05-29 11:00:20 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *main - contains our code to prints our output
  *Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
