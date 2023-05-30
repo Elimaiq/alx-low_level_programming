f98564189ab7531f972ecd3f2d7503ce02be3000
Comfort Ogar <elimaiq@gmail.com>
2023-05-30 10:38:30 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - contains the code that prints our output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
